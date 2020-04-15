c9222e3dbfe439fa44b14a7b98a3ef875afea704
ChrisMcCat <ichris.kk@gmail.com>
2020-04-15 11:02:16 +0200

    Added: -domain package with Trainer and TrainerRequest classes -freemarker package with Config and Template provider classes -dependencies for fm, hibernate, javax, mysql -.gitignore

---
+++ manager/src/main/java/domain/request/TrainerRequest.java
@@ -0,0 +1,44 @@
+package domain.request;
+
+public class TrainerRequest {
+
+    private String name;
+
+    private String surname;
+
+    private String phoneNumber;
+
+    private String mail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
