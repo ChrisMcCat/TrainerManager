c0f531ac2262c221c8ba03f44401d59c9675aa6d
ChrisMcCat <ichris.kk@gmail.com>
2020-04-17 06:16:37 +0200

    Updated Tracking class

--- Tracking/src/main/java/rest/Tracking.java
+++ Tracking/src/main/java/rest/Tracking.java
@@ -1,13 +1,14 @@
 package rest;
 
 import service.EventService;
+import util.JsonHelper;
 
 import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/action")
+@Path("/tracking")
 public class Tracking {
 
 	@Inject
@@ -17,12 +18,13 @@ public class Tracking {
 	@Path("event")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public void createNewActionFromJson(JsonObject json) {
+	public JsonObject createNewActionFromJson(JsonObject json) {
 		String ip = json.getString("ip");
 		String type = json.getString("type");
 		String link = json.getString("link");
 		String time = json.getString("time");
-		eventService.addEvent(ip, type, link, time);
+		long id = eventService.addEvent(ip, type, link, time);
+		return JsonHelper.toJson("id", id);
 	}
 
 	@GET
